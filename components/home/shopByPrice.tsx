04489615162d3539b6432e5c6276c44049e08240
Supriya Mitnala <mitnalasupriya@gmail.com>
2026-03-20 12:21:06 +0530

    added imges and created fl

---
+++ components/home/shopByPrice.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function ShopByPrice() {
+  return (
+    <div>
+         <h1 className='text-2xl font-bold text-black text-center my-4'>Shop By Price</h1>
+    </div>
+  )
+}
